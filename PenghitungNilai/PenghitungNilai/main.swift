d34f0cd1303903b4aa2fbee8594486036f4b1e16
Gilang Ramadhan <13895993+gilangadhan@users.noreply.github.com>
2020-09-11 16:15:22 +0700

    Update main.swift

--- PenghitungNilai/PenghitungNilai/main.swift
+++ PenghitungNilai/PenghitungNilai/main.swift
@@ -34,25 +34,25 @@ if let totalItem : Int = Int(total){
     var grade = ""
     
     switch average {
-    case 0..<40:
+    case 0...40:
         grade = "E";
         break
-    case 41..<50:
+    case 41...50:
         grade = "D";
         break
-    case 51..<60:
+    case 51...60:
         grade = "C";
         break
-    case 61..<70:
+    case 61...70:
         grade = "C+";
         break
-    case 71..<80:
+    case 71...80:
         grade = "B";
         break
-    case 81..<90:
+    case 81...90:
         grade = "B+";
         break
-    case 91..<100:
+    case 91...100:
         grade = "A";
         break
     default:
