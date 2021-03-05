cbb54796062cc898327bac9a8ed734ecb5fcb30a
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-03-05 17:03:01 +0700

    Update main.swift

--- DataDiri/DataDiri/main.swift
+++ DataDiri/DataDiri/main.swift
@@ -8,24 +8,24 @@
 
 import Foundation
 
-// Kita akan membuat form data diri
+// Kita akan membuat form data diri.
 print("Selamat Datang di Dicoding Academy")
 print("----------------------------------")
 
-// Kode ini digunakan untuk memasukkan input dari pengguna
-print("Masukkan nama depan Anda:");    let firstName = readLine()!
+// Kode ini digunakan untuk memasukkan input dari pengguna.
+print("Masukkan nama depan Anda:"); let firstName = readLine()!
 print("Masukkan nama belakang Anda:"); let lastName = readLine()!
-print("Masukkan umur Anda:");          let age = readLine()!
-print("Masukkan alamat Anda:");        let address = readLine()!
-print("Masukkan pekerjaan Anda:");     let job = readLine()!
+print("Masukkan umur Anda:"); let age = readLine()!
+print("Masukkan alamat Anda:"); let address = readLine()!
+print("Masukkan pekerjaan Anda:"); let job = readLine()!
 
 let fullName = firstName + " " + lastName
 
 print("----------------------------------")
 
-// Ini adalah output dari program yang Anda buat
+// Ini adalah output dari program yang Anda buat.
 print("Apakah kalian tahu \(fullName)?")
-print("\(firstName) adalah seorang \(job)")
-print("Saat ini ia berumur \(age) dan bertempat tinggal di \(address)")
+print("\(firstName) adalah seorang \(job).")
+print("Saat ini ia berumur \(age) dan bertempat tinggal di \(address).")
 
 print("----------------------------------")
