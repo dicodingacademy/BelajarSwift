2ddc6009b90f2febb3bf953948336f4a434b94e2
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-03-08 15:01:31 +0700

    Update main.swift

--- BermainKata/BermainKata/main.swift
+++ BermainKata/BermainKata/main.swift
@@ -7,27 +7,25 @@
 //
 
 import Foundation
-
 print("Selamat Datang di Dicoding Academy!")
 print("Mari bermain kata!")
 print("----------------------------------")
 
-// Digunakan untuk mengambil data inputan
+// Digunakan untuk mengambil data inputan.
 print("Masukkanlah nama depan Anda:"); let firstName = String(readLine() ?? "")
 print("Masukkanlah nama belakang Anda:"); let lastName = String(readLine() ?? "")
-
 print("----------------------------------")
-print("Berikut ini adalah operator-operator yang digunakan")
+print("Berikut ini adalah operator-operator yang digunakan:")
 
 // Operator Concatenation
 let concatenationString = firstName + lastName
-print("String Concatenation Operator : \"\(concatenationString)\"")
+print("1. String Concatenation Operator : \"\(concatenationString)\"")
 
 // Operator Mutability
 var mutabilityString = ""
 mutabilityString += firstName
 mutabilityString += lastName
-print("String Mutability Operator : \"\(mutabilityString)\"")
+print("2. String Mutability Operator : \"\(mutabilityString)\"")
 
 // Operator Comparison
 var comparisonString = ""
@@ -36,51 +34,49 @@ if firstName == lastName {
 } else {
     comparisonString = String(false)
 }
-print("String Comparison Operator : \"\(comparisonString)\"")
-
+print("3. String Comparison Operator : \"\(comparisonString)\"")
 print("----------------------------------")
-print("Berikut ini adalah fungsi-fungsi yang digunakan")
+print("Berikut ini adalah fungsi-fungsi yang digunakan:")
 
-// Menggabungkan nama depan dan nama belakang
+// Menggabungkan nama depan dan nama belakang.
 let fullName = firstName + " " + lastName
 
 // Fungsi isEmpty
 let empty = firstName.isEmpty || lastName.isEmpty
-print("Fungsi isEmpty : \"\(empty)\"")
+print("1. Fungsi isEmpty : \"\(empty)\"")
 
 // Fungsi startIndex
 let startIndex = fullName[fullName.startIndex]
-print("Fungsi startIndex : \"\(startIndex)\"")
+print("2. Fungsi startIndex : \"\(startIndex)\"")
 
 // Fungsi index
 let index = fullName[fullName.index(after: fullName.startIndex)]
-print("Fungsi index : \"\(index)\"")
+print("3. Fungsi index : \"\(index)\"")
 
 // Fungsi index Lainnya
 let customIndex = fullName[fullName.index(fullName.startIndex, offsetBy: 5)]
-print("Index ke lima : \"\(customIndex)\"")
+print("4. Index ke lima : \"\(customIndex)\"")
 
 // Fungsi endIndex
 let endIndex = fullName[fullName.index(before: fullName.endIndex)]
-print("Fungsi endIndex : \"\(endIndex)\"")
+print("5. Fungsi endIndex : \"\(endIndex)\"")
 
 // Fungsi insert
 var insert = fullName
 insert.insert("!", at:insert.endIndex)
-print("Fungsi insert : \"\(insert)\"")
+print("6. Fungsi insert : \"\(insert)\"")
 
 // Fungsi remove
 var remove = insert
 remove.remove(at: remove.index(before: remove.endIndex))
-print("Fungsi remove : \"\(remove)\"")
+print("7. Fungsi remove : \"\(remove)\"")
 
 // Fungsi append
 var append = firstName
 append.append(lastName)
-print("Fungsi append : \"\(append)\"")
+print("8. Fungsi append : \"\(append)\"")
 
 // Fungsi count
 let count = fullName.count
-print("Fungsi lenght : \(count)")
-
+print("9. Fungsi length : \(count)")
 print("----------------------------------")
