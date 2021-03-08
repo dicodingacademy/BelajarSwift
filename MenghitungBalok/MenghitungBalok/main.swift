32b8765a2a81d03f26e41781ad5cfda77a7f4e0b
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2021-03-08 14:46:49 +0700

    Update main.swift

--- MenghitungBalok/MenghitungBalok/main.swift
+++ MenghitungBalok/MenghitungBalok/main.swift
@@ -8,36 +8,36 @@
 
 import Foundation
 
-// Kita akan membuat form menghitung balok
-print("Selamat Datang di Dicoding Academy")
+import Foundation
 
-print("----------------------------------")
-// Kode ini digunakan untuk memasukkan input dari pengguna
+// Kita akan membuat form menghitung balok.
+print("Selamat Datang di Dicoding Academy!")
+
+// Kode ini digunakan untuk memasukkan input dari pengguna.
 print("Masukkanlah lebar balok:"); let widthInput = readLine()
 print("Masukkanlah tinggi balok:"); let heightInput = readLine()
 print("Masukkanlah panjang balok:"); let lengthInput = readLine()
 
-// Mengubah dari string ke int / double
-if let length = Double(lengthInput ?? "0"), let height = Double(heightInput ?? "0"), let width = Double(widthInput ?? "0"){
-    // Ini adalah proses penghitungan yang dilakukan dari program yang Anda buat
+// Mengubah dari string ke double
+if let length = Double(lengthInput ?? "0"), let height = Double(heightInput ?? "0"), let width = Double(widthInput ?? "0") {
+    // Ini adalah proses penghitungan yang dilakukan dari program yang Anda buat.
     let volume = length * height * width
     let surfaceArea = 2 * ((width * length) + (width * height) + (height * length))
     let circumference = 4 * (width + length + height)
-
     print("----------------------------------")
-    // Ini adalah output dari program yang Anda buat
+
+    // Ini adalah output dari program yang Anda buat.
     print("Anda memiliki sebuah balok dengan:")
-    print("Lebarnya adalah \(width) cm")
-    print("Tingginya adalah \(height) cm")
-    print("Panjangnya adalah \(length) cm")
-    print("Volemenya adalah \(volume) cm3")
-    print("Luas permukaannya adalah \(surfaceArea) cm2")
-    print("Kelilingnya adalah \(circumference) cm")
+    print("1. Lebarnya adalah \(width) cm.")
+    print("2. Tingginya adalah \(height) cm.")
+    print("3. Panjangnya adalah \(length) cm.")
+    print("4. Volemenya adalah \(volume) cm3.")
+    print("5. Luas permukaannya adalah \(surfaceArea) cm2.")
+    print("6. Kelilingnya adalah \(circumference) cm.")
     print("----------------------------------")
 } else {
-    // Ketika input tidak valid
+    // Ketika input tidak valid.
     print("----------------------------------")
-    print("Input tidak valid")
+    print("Input tidak valid!")
     print("----------------------------------")
 }
-
