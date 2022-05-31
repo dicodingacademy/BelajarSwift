9af6530bc46639363cdaf8a01d22328fea567858
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-05-31 16:02:35 +0700

    Update Kode

--- LatihanSwiftDasar/Data Diri/main.swift
+++ LatihanSwiftDasar/Data Diri/main.swift
@@ -1,31 +1,29 @@
 //
 //  main.swift
-//  DataDiri
+//  Data Diri
 //
-//  Created by Gilang Ramadhan on 24/10/19.
-//  Copyright © 2019 Dicoding Indonesia. All rights reserved.
+//  Created by Gilang Ramadhan on 31/05/22.
 //
 
 import Foundation
 
-// Kita akan membuat form data diri.
+// MARK: Membuat form data diri.
 print("Selamat Datang di Dicoding Academy")
 print("----------------------------------")
 
-// Kode ini digunakan untuk memasukkan input dari pengguna.
+// MARK: Memasukkan nilai dari pengguna.
 print("Masukkan nama depan Anda:"); let firstName = readLine()!
 print("Masukkan nama belakang Anda:"); let lastName = readLine()!
 print("Masukkan umur Anda:"); let age = readLine()!
 print("Masukkan alamat Anda:"); let address = readLine()!
 print("Masukkan pekerjaan Anda:"); let job = readLine()!
 
+// MARK: Menggabungkan nama depan dan nama belakang.
 let fullName = firstName + " " + lastName
-
 print("----------------------------------")
 
-// Ini adalah output dari program yang Anda buat.
+// MARK: Mencetak program yang Anda buat ke dalam Terminal.
 print("Apakah kalian tahu \(fullName)?")
 print("\(firstName) adalah seorang \(job).")
 print("Saat ini ia berumur \(age) dan bertempat tinggal di \(address).")
-
 print("----------------------------------")
