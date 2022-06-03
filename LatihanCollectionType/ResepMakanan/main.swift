f0ded877d8ad3a48db1c3fd7e2dcbecef2d07715
Gilang Ramadhan <gilangramadhan96.gr@gmail.com>
2022-06-03 16:36:40 +0700

    Update Code

--- LatihanCollectionType/ResepMakanan/main.swift
+++ LatihanCollectionType/ResepMakanan/main.swift
@@ -8,35 +8,45 @@
 
 import Foundation
 
+import Foundation
+
 print("Selamat Datang di Dicoding Resto!")
 print("Mari membuat makanan!")
 
+// MARK: Memasukkan resep dan jumlah item dalam resep tersebut.
 print("----------------------------------")
-
-print("Apa yang ingin Anda buat?");let food = readLine() ?? ""
+print("Apa yang ingin Anda buat?"); let food = readLine() ?? ""
 print("Masukkan jumlah item yang ingin Anda masukkan", terminator: ": "); let newItem = readLine() ?? "0"
-
 print("----------------------------------")
 
-if let totalItem : Int = Int(newItem) {
-    var recipe = [String]()
-    for index in 1...totalItem {
-        print("Masukkanlah item ke \(index)", terminator: ": "); let newItem = String(readLine() ?? "")
-        recipe.append(newItem)
-    }
-    
-    print("----------------------------------")
-    
-    print("Berapa lama proses memasak dilakukan?");let time = readLine() ?? ""
-    
-    print("----------------------------------")
-    recipe.sort()
-    print("Anda akan membuat \(food) selama \(time) dengan resep:")
-    for (index, value) in recipe.enumerated() {
-        print("\(index + 1). \(value)")
-    }
-    
+// MARK: Mengonversi jumlah item yang perlu dimasukkan.
+if let totalItem: Int = Int(newItem) {
+  var recipe = [String]()
+
+  // MARK: Memasukkan item ke dalam resep.
+  for index in 1...totalItem {
+    print("Masukkanlah item ke \(index)", terminator: ": "); let newItem = String(readLine() ?? "")
+    recipe.append(newItem)
+  }
+
+  // MARK: Memasukkan waktu yang dibutuhkan untuk membuat resep.
+  print("----------------------------------")
+  print("Berapa lama proses memasak dilakukan?"); let time = readLine() ?? ""
+  print("----------------------------------")
+
+  // MARK: Mengurutkan Item Resep.
+  recipe.sort()
+
+  // MARK: Menampilkan hasil resep.
+  print("Anda akan membuat \(food) selama \(time) dengan resep:")
+  for (index, value) in recipe.enumerated() {
+    print("\(index + 1). \(value)")
+  }
+  
 } else {
-    print("Input tidak valid")
+
+  // MARK: Menampilkan Error ketika input tidak valid.
+  print("Input tidak valid!")
+
 }
 print("----------------------------------")
